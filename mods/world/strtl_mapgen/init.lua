78d08014bd496325f537c0fa3aef6637f4dea888
Crystal Melting Dot <stresspassing@gmail.com>
2022-03-24 06:56:24 +0300

    naming conventions compliance fix

---
+++ mods/world/strtl_mapgen/init.lua
@@ -0,0 +1,5 @@
+
+-- Essential aliases
+minetest.register_alias("mapgen_stone", "strtl_corenodes:stone")
+minetest.register_alias("mapgen_water_source", "strtl_corenodes:water_source")
+minetest.register_alias("mapgen_river_water_source", "strtl_corenodes:water_source")
