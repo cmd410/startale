c7843ade24b123ca18bab30de5592fee77c46933
Crystal Melting Dot <stresspassing@gmail.com>
2022-03-25 15:31:53 +0300

    merge liquids and solids into strtl_nodes mod

--- mods/world/strtl_mapgen/init.lua
+++ mods/world/strtl_mapgen/init.lua
@@ -1,22 +1,22 @@
 
 -- Essential aliases
-minetest.register_alias("mapgen_stone", "strtl_solids:stone")
-minetest.register_alias("mapgen_water_source", "strtl_liquids:water_source")
-minetest.register_alias("mapgen_river_water_source", "strtl_liquids:river_water_source")
+minetest.register_alias("mapgen_stone", "strtl_nodes:stone")
+minetest.register_alias("mapgen_water_source", "strtl_nodes:water_source")
+minetest.register_alias("mapgen_river_water_source", "strtl_nodes:river_water_source")
 
 minetest.clear_registered_biomes()
 minetest.clear_registered_decorations()
 
 minetest.register_biome({
     name = "mapgen:grassland",
-    node_top = "strtl_solids:dirt_with_grass",
+    node_top = "strtl_nodes:dirt_with_grass",
     depth_top = 1,
-    node_filler = "strtl_solids:dirt",
+    node_filler = "strtl_nodes:dirt",
     depth_filler = 2,
-    node_riverbed = "strtl_solids:sand",
+    node_riverbed = "strtl_nodes:sand",
     depth_riverbed = 2,
-    --node_dungeon = "strtl_solids:cobble",
-    --node_dungeon_alt = "strtl_solids:cobble",
+    --node_dungeon = "strtl_nodes:cobble",
+    --node_dungeon_alt = "strtl_nodes:cobble",
     --node_dungeon_stair = "stairs:stair_cobble",
     y_max = 31000,
     y_min = 4,
---
+++ mods/world/strtl_nodes/init.lua
@@ -0,0 +1,39 @@
+local modname = minetest.get_current_modname()
+local modpath = minetest.get_modpath(modname)
+
+dofile(modpath .. "/liquids.lua")
+
+minetest.register_node("strtl_nodes:cobble", {
+	description = "Cobble",
+	tiles = {"strtl_nodes_cobble.png"},
+	groups = {dig_immediate=2, cracky=3},
+})
+
+minetest.register_node("strtl_nodes:stone", {
+	description = "Stone",
+	tiles = {"strtl_nodes_stone.png"},
+	groups = {dig_immediate=2, cracky=3},
+	drop = "strtl_nodes:cobble"
+})
+
+minetest.register_node("strtl_nodes:sand", {
+	description = "Sand",
+	tiles = {"strtl_nodes_sand.png"},
+	groups = {dig_immediate=2},
+})
+
+minetest.register_node("strtl_nodes:dirt", {
+	description = "Dirt",
+	tiles = {"strtl_nodes_dirt.png"},
+	groups = {dig_immediate=2, soil=1},
+})
+
+minetest.register_node("strtl_nodes:dirt_with_grass", {
+	description = "Dirt with Grass",
+	tiles ={"strtl_nodes_grass.png",
+		"strtl_nodes_dirt.png",
+		{name = "strtl_nodes_dirt.png^strtl_nodes_grass_slide.png",
+		tileable_vertical = false}},
+	groups = {dig_immediate=2, soil=1},
+	drop = "strtl_nodes:dirt"
+})
