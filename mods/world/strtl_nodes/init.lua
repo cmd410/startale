c8cfae4c75ce8d6cff01a2bc35c80bb9fa1a8b24
Crystal Melting Dot <stresspassing@gmail.com>
2022-03-26 10:58:56 +0300

    fix Mossy cobble description

--- mods/world/strtl_nodes/init.lua
+++ mods/world/strtl_nodes/init.lua
@@ -10,7 +10,7 @@ minetest.register_node("strtl_nodes:cobble", {
 })
 
 minetest.register_node("strtl_nodes:mossycobble", {
-	description = "Cobble",
+	description = "Mossy cobble",
 	tiles = {"strtl_nodes_mossycobble.png"},
 	groups = {dig_immediate=2, cracky=3},
 })
