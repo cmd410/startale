4f8a0854d3be070765a10283cd2a82ce31ffbb2f
Crystal Melting Dot <stresspassing@gmail.com>
2022-04-10 21:54:13 +0300

    greater max_elevation for trees spawning

--- mods/world/st_mapgen/decorations/trees.lua
+++ mods/world/st_mapgen/decorations/trees.lua
@@ -13,6 +13,7 @@ biome_lib.register_on_generate({
     plantlife_limit=0.6,
     rarity=15,
     max_count=1,
+    max_elevation=31000,
     delete_above=true,
     delete_above_surround=true,
 }, treedef)
