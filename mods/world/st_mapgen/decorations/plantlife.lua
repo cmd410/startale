2c5e08e42ea56af0c1096cbc832a865ce8756d1a
Crystal Melting Dot <stresspassing@gmail.com>
2022-04-10 21:53:37 +0300

    plantlife tweaks

--- mods/world/st_mapgen/decorations/plantlife.lua
+++ mods/world/st_mapgen/decorations/plantlife.lua
@@ -13,10 +13,25 @@ local grass_biome = {
     spawn_surfaces = grow_plants_on,
     surface=grow_plants_on,
     spawn_delay=60,
-    spawn_chance=50,
+    spawn_chance=15,
     light_min=3,
-    rarity=15
+    rarity=3,
+    max_count=125,
+
+    min_elevation=1,
+    max_elevation=31000
 }
 
 biome_lib.register_active_spawner(grass_biome)
-biome_lib.register_on_generate(grass_biome, grass_nodes)
+
+--biome_lib.register_on_generate(grass_biome, grass_nodes)
+-- Registering as minetest decoration because
+-- biome_lib.register_on_generate just doesn't cut it density-wise
+minetest.register_decoration(
+    {
+        deco_type="simple",
+        decoration=grass_nodes,
+        place_on=grow_plants_on,
+        fill_ratio=0.6
+    }
+)
